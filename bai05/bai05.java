84d7de0534c2fd709bbb867a3f6764844e4beac1
trungkodz <ntt2007qbhg@gmail.com>
2026-02-02 23:35:40 +0700

    ghi&docfile

--- bai05/bai05.java
+++ bai05/bai05.java
@@ -3,6 +3,7 @@ package bai05;
 import java.util.Scanner;
 import java.lang.Math;
 import java.lang.reflect.Array;
+import java.io.*;
 
 public class bai05 {
     public static void main(String[] args) {
@@ -12,9 +13,24 @@ public class bai05 {
         double dlt1 = sc.nextDouble();
         double dth1 = sc.nextDouble();
         sc.nextLine();
+        String msv2 = sc.nextLine();
+        String hoten2 = sc.nextLine();
+        double dlt2 = sc.nextDouble();
+        double dth2 = sc.nextDouble();
+        sc.nextLine();
 
         sinhvien sv1 = new sinhvien(msv1, hoten1, dlt1, dth1);
 
         sv1.thongtinsv();
+        sv1.ghifile(hoten1);
+
+        sinhvien svm = new sinhvien(msv2, hoten2, dlt2, dth2);
+
+        System.out.println("DANG GHI FILE!>>>");
+        svm.ghifile(hoten2);// Ghi vao file
+        sinhvien sv2 = new sinhvien();
+        System.out.println("DANG DOC FILE!>>>");
+        sv2.doctufile(hoten2);// doc tu file
+        sv2.thongtinsv();
     }
 }
--- bai05/sinhvien.java
+++ bai05/sinhvien.java
@@ -1,8 +1,15 @@
 package bai05;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class sinhvien {
     private String msv, hoten;
     private double dlt, dth;
+
+    public sinhvien() {
+
+    }
 
     public sinhvien(String msv, String hoten, double dlt, double dth) {
         this.msv = msv;
@@ -10,6 +17,14 @@ public class sinhvien {
         this.dlt = dlt;
         this.dth = dth;
 
+    }
+
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+        this.msv = sc.nextLine();
+        this.hoten = sc.nextLine();
+        this.dlt = sc.nextDouble();
+        this.dth = sc.nextDouble();
     }
 
     public double dtb() {
@@ -30,6 +45,58 @@ public class sinhvien {
         System.out.println("dlt: " + this.dlt);
         System.out.println("dth: " + this.dth);
 
-        System.out.println("ket qua hoc tap:  " + ketqua() + " diem tb: " + dtb());
+        System.out.println("ket qua hoc tap: " + ketqua() + " diem tb: " + dtb());
+    }
+
+    // hàm ghi thông tin đối tượng hiện tại vào file
+    public void ghifile(String tenfile) {
+        try {
+            FileWriter fw = new FileWriter(tenfile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            // ghi vào file
+            bw.write(this.msv);
+            bw.newLine();// xuống dòng
+            bw.write(this.hoten);
+            bw.newLine();
+            bw.write(String.valueOf(this.dlt));
+            bw.newLine();
+            bw.write(String.valueOf(this.dth));
+
+            bw.close(); // phải đóng file mới lưu dữ liệu
+            System.out.println("du lieu da in vao " + tenfile + " thanh cong!");
+        } catch (IOException e) {
+            System.out.println("loi khi ghi file: " + e.getMessage());
+        }
+
+    }
+
+    public void doctufile(String tenfile) {
+        try {
+            File f = new File(tenfile);
+            if (!f.exists()) {
+                System.out.println("file ko ton tai!");
+                return;
+            }
+
+            Scanner sc = new Scanner(f);
+
+            if (sc.hasNextLine()) {
+                this.msv = sc.nextLine();
+            }
+            if (sc.hasNextLine()) {
+                this.hoten = sc.nextLine();
+            }
+            if (sc.hasNextLine()) {
+                this.dlt = Double.parseDouble(sc.nextLine());
+            }
+            if (sc.hasNextLine()) {
+                this.dth = Double.parseDouble(sc.nextLine());
+            }
+            sc.close();
+            System.out.println("doc du lieu thanh cong! ");
+
+        } catch (Exception e) {
+            System.out.println("ko doc dc file: " + e.getMessage());
+        }
     }
 }
