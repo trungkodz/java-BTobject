4bdbbdcac09a138dcc3eaedcde1b472fe2500554
trungkodz <ntt2007qbhg@gmail.com>
2026-02-12 00:42:32 +0700

    donebai08

--- bai08/CircleCollection.java
+++ bai08/CircleCollection.java
@@ -1,12 +1,84 @@
 package bai08;
 
-public class CircleCollection extends Circle {
-    public CircleCollection(double radius) {
-        super(radius);
+import java.util.Arrays;
+
+public class CircleCollection {
+    private Circle[] hinhtron;
+
+    public CircleCollection() {
+        this.hinhtron = new Circle[0];
     }
 
-    public void addCircle(double radius) {
-        double manghtron[] = { radius };
+    public void addCircle(Circle a) {
+        this.hinhtron = Arrays.copyOf(this.hinhtron, this.hinhtron.length + 1);
+        this.hinhtron[this.hinhtron.length - 1] = a;
     }
-    
+
+    public int getSize() {
+        return (this.hinhtron.length);
+    }
+
+    public Circle getCircle(int getc) {
+        if (getc >= 0 && getc < this.hinhtron.length) {
+            return this.hinhtron[getc];
+        } else {
+            return null;
+        }
+    }
+
+    public Circle setCircle(int setc, Circle a) {
+        if (setc >= 0 && setc < this.hinhtron.length) {
+            return this.hinhtron[setc] = a;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String s = " ";
+        for (int i = 0; i < this.hinhtron.length; i++) {
+            s += "hinh " + (i + 1) + ": " + "\n" + hinhtron[i].toString() + "\n";
+        }
+        return s;
+    }
+
+    public double tong() {
+        double tong = 0;
+        for (int i = 0; i < this.hinhtron.length; i++) {
+            tong += hinhtron[i].Area();
+        }
+        return tong;
+    }
+
+    public double max() {
+        double max = hinhtron[0].Area();
+        for (int i = 0; i < this.hinhtron.length; i++) {
+            if (hinhtron[i].Area() > max) {
+                max = hinhtron[i].Area();
+            }
+        }
+        return max;
+    }
+
+    public double min() {
+        double min = hinhtron[0].Area();
+        for (int i = 0; i < this.hinhtron.length; i++) {
+            if (hinhtron[i].Area() < min) {
+                min = hinhtron[i].Area();
+            }
+        }
+        return min;
+
+    }
+
+    public Circle min1() {
+        Circle min = hinhtron[0];
+        for (int i = 0; i < this.hinhtron.length; i++) {
+            if (hinhtron[i].Area() < min.Area()) {
+                min = hinhtron[i];
+            }
+        }
+        return min;
+    }
 }
--- bai08/bai08.java
+++ bai08/bai08.java
@@ -9,38 +9,21 @@ public class bai08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        Circle h1 = new Circle(3.6);
-        System.out.println(h1);
-
-        int n;
-        double tong = 0;
-        System.out.print("so hinh tron la: ");
-        n = sc.nextInt();
-        Circle mang[] = new Circle[n];
-        System.out.println("thong tin cac hinh: ");
-        for (int i = 0; i < n; i++) {
-            double radiusrand = rand.nextDouble(5, 100);
-            mang[i] = new Circle(radiusrand);
+        int N = sc.nextInt();
+        CircleCollection hinhtron = new CircleCollection();
+        for (int i = 0; i < N; i++) {
+            double rd = rand.nextDouble(1, 100);
+            Circle ht = new Circle(rd);
+            hinhtron.addCircle(ht);
         }
 
-        double max = mang[0].Area();
-        Circle min = mang[0];
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("hinh " + (i + 1));
-            System.out.println(mang[i]);
-            tong = tong + mang[i].Area();
-            if (mang[i].Area() > max) {
-                max = mang[i].Area();
-            }
-            if (mang[i].getradius() < min.getradius()) {
-                min = mang[i];
-            }
-        }
-
-        System.out.println("tong Area: " + tong);
-        System.out.println("Area max = " + max);
-        System.out.println("thong tin HT min: " + "\n" + min);
+        System.out.println("thong tin cac hinh da nhap:");
+        System.out.println(hinhtron);
+        System.out.println("tong Area: " + hinhtron.tong());
+        System.out.println("max la: " + hinhtron.max());
+        System.out.println("min la: " + hinhtron.min());
+        Circle minc = hinhtron.min1();
+        System.out.println("hinh tron min: " + "\n" + minc.toString());
 
     }
 }
